8f63b66eca641dd765bbbf1176965b0bf4bc59d3
phishfish <lucialwang@gmail.com>
2023-09-19 15:32:25 -0400

    Gaslight Gatekeep Girlboss

---
+++ C/Gaslight-Search.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isSorted(int *array)
+{
+	return true;
+}
+
+int main(void)
+{
+	int numInts;
+
+	printf("How many integers to be sorted?");
+	scanf(%d, &numInts);
+	
+	return 0;
+}
